fdd4edf6af0d9efd7245f7e7e77d795d8a38863c
Nikita <62756604+FlamesONE@users.noreply.github.com>
2021-03-21 18:30:06 +0500

    Update General.php

--- app/ext/General.php
+++ app/ext/General.php
@@ -196,7 +196,7 @@ class General {
      */
     public function get_default_options() {
         $options = file_exists( SESSIONS . '/options.php' ) ? require SESSIONS . '/options.php' : null;
-        return empty( $options['animations'] ) ? exit(require 'app/page/custom/install/index.php') : $options;
+        return !isset( $options['animations'] ) ? exit(require 'app/page/custom/install/index.php') : $options;
     }
 
     /**
